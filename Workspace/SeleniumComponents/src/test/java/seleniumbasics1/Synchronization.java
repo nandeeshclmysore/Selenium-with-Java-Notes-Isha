ad969c9cf3f66ab3f46fa6194f8b8dfae6bfa6b1
nandeesh.apjkalam@gmail.com <nandeesh.apjkalam@gmail.com>
2025-06-22 07:39:06 +0530

    Day 33 Selenium

--- Workspace/SeleniumComponents/src/test/java/seleniumbasics1/Synchronization.java
+++ Workspace/SeleniumComponents/src/test/java/seleniumbasics1/Synchronization.java
@@ -1,7 +1,13 @@
 package seleniumbasics1;
 
+import framework.SeleniumUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -9,7 +15,8 @@ public class Synchronization {
 
     RemoteWebDriver driver;
 
-    public void implicitlyWaitDemo() {
+    @Test
+    public void implicitlyAndExplicitWaitDemo() throws InterruptedException {
         driver = new ChromeDriver();
 
         //To get all default capabilities of the opened driver
@@ -17,6 +24,31 @@ public class Synchronization {
 
         //implicit wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //Create object for Util class
+        SeleniumUtils seleniumUtils = new SeleniumUtils(driver);
+
+        //Using Utils class object call the utilities methods
+        seleniumUtils.launchApp("https://practicetestautomation.com/practice-test-login/");
+
+        //Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        //By as argument
+        WebElement emailTextbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("username")));
+        //WebElement as argument
+        WebElement passwordTextbox = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("password"))));
+        WebElement signinButton = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("submit"))));
+
+        seleniumUtils.enterDataOnTextBox(emailTextbox, "student", "emailTextbox");
+        seleniumUtils.enterDataOnTextBox(passwordTextbox, "Password123", "passwordTextbox");
+
+
+        seleniumUtils.clickOnElement(signinButton, "signinButton");
+
+        Thread.sleep(3000);
+
+        seleniumUtils.closeBrowser();
     }
 
 }

a26b4c62de171d485a8ce5a68e060fed708b31aa
nandeesh.apjkalam@gmail.com <nandeesh.apjkalam@gmail.com>
2025-08-07 13:08:34 +0530

    Rest assured day 1 java

--- RestAssuredWorkspace/RestAssuredComponenets/src/test/java/JavaForRestassured/HeloWorld.java
+++ RestAssuredWorkspace/RestAssuredComponenets/src/test/java/JavaForRestassured/HeloWorld.java
@@ -1,12 +1,12 @@
 package JavaForRestassured;
 
-public class HeeloWorld {
+public class HeloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World !!");
         System.out.println(555 + 888 - 222 * 6666);
 
-        //Left to Right Execution: BODMAS Rule
+        //BODMAS Rule else default execution is Left to Right Execution
         System.out.println("Sum of " + 555 + 555);  //Sum of 555555 -> Left to Right execution so Concatenation
         System.out.println(555 + 555+" Sum is"); //1110 Sum is
         System.out.println(555 + 555+" Sum is"+6666+666); //1110 Sum is6666666
